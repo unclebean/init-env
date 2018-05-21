772362f1f53bf8b76feaafad7b2096257f85ecfb
unclebean <unclebean2013@gmail.com>
2018-05-21 15:59:36 +0800

    resovle jenkins devops issue finally

--- bin/setup_prod_in_jenkins.sh
+++ bin/setup_prod_in_jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook ./playbooks/main_ansible2.2.yml -i ./production/inventory --private-key=/home/jenkins/.ssh/developer_rsa -u root --extra-vars '@secrets/passwd.yml' --vault-password-file=secrets/vault.txt
+ansible-playbook ./playbooks/main_ansible2.2.yml -i ./production/inventory -u root --extra-vars '@secrets/passwd.yml' --vault-password-file=secrets/vault.txt

b16532ec56534e5970862841691a48fbf75e7ff6
unclebean <unclebean2013@gmail.com>
2018-05-21 14:22:34 +0800

    update ansible

--- bin/setup_prod.sh
+++ bin/setup_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook ./playbooks/main.yml -i ./production/inventory --private-key=~/.ssh/unclebean_rsa -u root --extra-vars '@secrets/passwd.yml' --vault-password-file=secrets/vault.txt
+ansible-playbook ./playbooks/main.yml -i ./production/inventory --private-key=~/.ssh/developer_rsa -u root --extra-vars '@secrets/passwd.yml' --vault-password-file=secrets/vault.txt
--- bin/update_prod.sh
+++ bin/update_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # please note: after setup production environment should use update_prod.sh to update env because root user would be block through ssh
-ansible-playbook ./playbooks/main.yml -i ./production/inventory --private-key=~/.ssh/unclebean_rsa -u deploy --extra-vars '@secrets/passwd.yml' --vault-password-file=secrets/vault.txt
+ansible-playbook ./playbooks/main.yml -i ./production/inventory --private-key=~/.ssh/developer_rsa -u deploy --extra-vars '@secrets/passwd.yml' --vault-password-file=secrets/vault.txt

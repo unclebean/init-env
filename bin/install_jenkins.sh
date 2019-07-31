b14fe31437244c1eed6a22a3590645af2e44fa0c
unclebean <unclebean2013@gmail.com>
2019-08-01 07:50:43 +0800

    enhance install jenkins

--- bin/install_jenkins.sh
+++ bin/install_jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook ./playbooks/main.yml -i ./production/jenkins-inventory --private-key=~/.ssh/developer_rsa -u root --extra-vars '@secrets/passwd.yml' --vault-password-file=secrets/vault.txt
+ansible-playbook ./playbooks/main.yml -i ./production/jenkins-inventory --private-key=~/.ssh/developer_rsa -u deploy --extra-vars '@secrets/passwd.yml' --vault-password-file=secrets/vault.txt
